84aa132cc4490917bc1df30be8fe92c07b9b74b1
widmogrod <widmogrod@gmail.com>
2013-09-09 20:35:41 +0200

    basic math functions

---
+++ matematical.js
@@ -0,0 +1,9 @@
+(function(exports){
+
+    // in alphabetical order
+    exports.addition        = function(a, b) { return a + b; };
+    exports.subtraction     = function(a, b) { return a - b; };
+    exports.multiplication  = function(a, b) { return a * b; };
+    exports.division        = function(a, b) { return a / b; };
+
+})(exports || this);
