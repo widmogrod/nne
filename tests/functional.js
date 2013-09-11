8b8639bd2262794a7b6f443f4422bb16c801b2f2
widmogrod <widmogrod@gmail.com>
2013-09-11 23:41:27 +0200

    remove partial and use curry instead of it

--- tests/functional.js
+++ tests/functional.js
@@ -13,33 +13,69 @@ var result = f.map(data, function(item){ return '+' + item});
 console.log(result);
 
 // Two
-var concatPlus = f.partial(concat, '+');
+var concatPlus = f.curry(concat, '+');
 
 var result = f.map(data, concatPlus);
 console.log(result);
 
 // Three
-var contactUndersocre = f.partial(concat, '_');
+var contactUndersocre = f.curry(concat, '_');
 var contactByPlusAndUndersoce = f.compose(concatPlus, contactUndersocre);
 
 var result = f.map(data, contactByPlusAndUndersoce);
 console.log(result);
 
 // Four
+var abc = function(a, b, c) {
+    return "$a = " + a + " $b = " + b + " $c = " + c;
+}
+
+var result = f.curry(abc);
+console.log(result);
+result = result(1);
+console.log(result);
+result = result(2);
+console.log(result);
+result = result(3);
+console.log(result);
+
+// Five
+function z(func) {
+    return function() {
+        return func.apply(null, f.slice(arguments));
+    }
+};
+
 var summation = function(data, func) {
-    var base = f.partial(f.reduce, data);
-    var reduce = f.partial(base, 0);
-    return reduce(function(base, item) {
-        return m.addition(base, func(item));
-    });
+    return f.reduce(
+        data,
+        f.compose(m.addition, func, f.rfunc),
+        0
+    );
 }
 var result = summation(datanum, function(item) {
-    return item;
+    return ++item;
 });
 console.log(result);
+
 
 // Five
 var result = m.summation(datanum, function(item) {
     return item;
 });
 console.log(result);
+
+// Six
+
+
+
+
+
+
+
+
+
+
+
+
+
