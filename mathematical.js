b987dff234d81cff4edfdfd6a328d022143deafc
widmogrod <widmogrod@gmail.com>
2013-09-14 22:03:39 +0200

    base mathematical operations now can accept more that two arguments

--- mathematical.js
+++ mathematical.js
@@ -3,10 +3,10 @@
 
     // in alphabetical order
     exports.equal           = function(a, b) { return a === b; };
-    exports.addition        = function(a, b) { return a + b; };
-    exports.subtraction     = function(a, b) { return a - b; };
-    exports.multiplication  = function(a, b) { return a * b; };
-    exports.division        = function(a, b) { return a / b; };
+    exports.addition        = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base + i}, a); };
+    exports.subtraction     = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base - i}, a); };
+    exports.multiplication  = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base * i}, a); };
+    exports.division        = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base / i}, a); };
 
     // ∑ - sum over data from ... to ... of func
     exports.summation = function(data, func) {
--- test/mathematical.js
+++ test/mathematical.js
@@ -3,26 +3,30 @@ var m = require('../mathematical.js');
 describe('Mathematical', function(){
     describe('#addition()', function(){
         it('should return result of operation if argument are passed', function(){
-            m.addition(1,2).should.be.eql(3);
-            m.addition(11,22).should.be.eql(33);
+            m.addition(1, 2).should.be.eql(3);
+            m.addition(11, 22).should.be.eql(33);
+            m.addition(11, 22, 10).should.be.eql(43);
         })
     })
     describe('#subtraction()', function(){
         it('should return result of operation if argument are passed', function(){
-            m.subtraction(1,2).should.be.eql(-1);
-            m.subtraction(11,22).should.be.eql(-11);
+            m.subtraction(1, 2).should.be.eql(-1);
+            m.subtraction(11, 22).should.be.eql(-11);
+            m.subtraction(11, 22, 10).should.be.eql(-21);
         })
     })
     describe('#multiplication()', function(){
         it('should return result of operation if argument are passed', function(){
-            m.multiplication(1,2).should.be.eql(2);
-            m.multiplication(10,22).should.be.eql(220);
+            m.multiplication(1, 2).should.be.eql(2);
+            m.multiplication(10, 22).should.be.eql(220);
+            m.multiplication(10, 22, 10).should.be.eql(2200);
         })
     })
     describe('#division()', function(){
         it('should return result of operation if argument are passed', function(){
-            m.division(1,2).should.be.eql(0.5);
-            m.division(10,2).should.be.eql(5);
+            m.division(1, 2).should.be.eql(0.5);
+            m.division(10, 2).should.be.eql(5);
+            m.division(10, 5, 2).should.be.eql(1);
         })
     })
     describe('#equal()', function(){
