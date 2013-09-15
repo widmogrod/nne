1f9c38398a3dcc7d37f7333b2eb8fe4fdb7cf052
widmogrod <widmogrod@gmail.com>
2013-09-15 15:50:38 +0200

    mathematical.js now is using named functions instead anonymous

--- mathematical.js
+++ mathematical.js
@@ -2,15 +2,36 @@
     var f = require('./functional.js');
 
     // in alphabetical order
-    exports.equal           = function(a, b) { return a === b; };
-    exports.addition        = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base + i}, a); };
-    exports.subtraction     = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base - i}, a); };
-    exports.multiplication  = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base * i}, a); };
-    exports.division        = function(a, b) { return f.reduce(f.slice(arguments, 1), function(i, base) {return base / i}, a); };
+    function equal(a, b) {
+        return a === b;
+    };
+
+    function addition(a, b) {
+        return f.reduce(f.slice(arguments, 1), function(i, base) {return base + i}, a);
+    };
+
+    function subtraction(a, b) {
+        return f.reduce(f.slice(arguments, 1), function(i, base) {return base - i}, a);
+    };
+
+    function multiplication(a, b) {
+        return f.reduce(f.slice(arguments, 1), function(i, base) {return base * i}, a);
+    };
+
+    function division(a, b) {
+        return f.reduce(f.slice(arguments, 1), function(i, base) {return base / i}, a);
+    };
 
     // ∑ - sum over data from ... to ... of func
-    exports.summation = function(data, func) {
+    function summation(data, func) {
         return f.reduce(data, f.compose(exports.addition, func || f.returnValue, f.returnValue), 0);
     };
+
+    exports.equal           = equal;
+    exports.addition        = addition;
+    exports.subtraction     = subtraction;
+    exports.multiplication  = multiplication;
+    exports.division        = division;
+    exports.summation       = summation;
 
 })(exports || this);
