2129b1dd76499dc9d24fbd3b05b1c510cc23d1f7
widmogrod <widmogrod@gmail.com>
2013-09-25 05:26:53 +0200

    streams and monads

--- monads-streams.js
+++ monads-streams.js
@@ -1,26 +1,100 @@
-function mValue(state) {
-    return function(func) {
-        return typeof func === 'function' ? func(mValue(state)) : state;
+(function(){
+    console.log('forward stream');
+
+    function mValue(state) {
+        return function(func) {
+            return typeof func === 'function' ? func(mValue(state)) : state;
+        }
     }
-}
+    function mAddOne(mv) {
+        return mValue(mv() + 1);
+    }
+    function mMinusOne(mv) {
+        return mValue(mv() - 1);
+    }
 
-function mAddOne(mv) {
-    return mValue(mv() + 1);
-}
+    function mDisplay(mv) {
+        console.log(mv())
+        return mv;
+    }
 
+    mValue(10)(mAddOne)(mAddOne)(mDisplay)(mAddOne)(mAddOne)(mDisplay);
 
-function mMinusOne(mv) {
-    return mValue(mv() - 1);
-}
+})();
 
+(function(){
+    console.log('reverse stream');
 
-function mDisplay(mv) {
-    console.log(mv())
-    return mv;
-}
+    function mValue(state) {
+        return function monadic() {
+            return state;
+        }
+    }
 
-mValue(10)(mAddOne)(mAddOne)(mDisplay)(mAddOne)(mAddOne)(mDisplay);
+    function postphone(func, next) {
+        if (next.name === 'monadic') {
+            return func(next);
+        }
 
-// var nn = (mFeedForward)(mCalculateDelta)(mUpdateWeights);
+        return function postphoned(forward) {
+            if (forward.name === 'monadic') {
+                return next(func(forward));
+            } else {
+                return postphone(
+                    postphone(func, next),
+                    forward
+                );
+            }
+        }
+    }
 
-// mValue(20)(nn);
+    function mAddOne(mv) {
+        if (mv.name === 'monadic') {
+            return mValue(mv() + 1) ;
+        }
+
+        return postphone(mAddOne, mv);
+    }
+
+    function mDisplay(mv) {
+        if (mv.name === 'monadic') {
+            console.log('mDisplay', mv());
+            return mv;
+        }
+
+        return postphone(mDisplay, mv);
+    }
+
+    function A(mv) {
+        console.log('A', mv());
+        return mv;
+    }
+    function B(mv) {
+        console.log('B', mv());
+        return mv;
+    }
+
+    console.log(postphone(A, B)(mValue(10)));
+
+    function C(write) {
+        if (write.name === 'monadic') {
+            console.log('C inside', write);
+            return mValue(write() + 1);
+        }
+
+        return postphone(C, read);
+    }
+
+    // console.log('C write', C(mValue(10)));
+    // console.log('C read', (C)(C)(mValue(10))());
+
+    console.log('short');
+    (mAddOne)(mDisplay)
+    (mValue(10));
+
+    console.log('long');
+    (mAddOne)(mAddOne)(mDisplay)
+    (mAddOne)(mAddOne)(mDisplay)
+    (mValue(10));
+
+})();
