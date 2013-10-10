1cd267b6c0ba96a6499bb18a32ed324b21abee1b
widmogrod <widmogrod@gmail.com>
2013-10-10 21:20:56 +0200

    add set function

--- functional.js
+++ functional.js
@@ -72,6 +72,19 @@
     }
 
     /**
+     * Set param in object property
+     * @param  {[type]}  key
+     * @return {Boolean}
+     */
+    function set(key) {
+        return function (obj) {
+            return function(value) {
+                obj[key] = value;
+            };
+        }
+    }
+
+    /**
      * Return first element from array
      */
     function first(array) {
